0f1f876bbb8852cf69bf9dc129bda228b577f491
Amelia Kanabaj <a.kanabaj.124@studms.ug.edu.pl>
2025-01-26 19:55:20 +0100

    added styles to Device and Devices components

--- moj_symulator/src/components/Device.jsx
+++ moj_symulator/src/components/Device.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import './styles/Device.css';
 
 export default function Device() {
     const navigate = useNavigate();
@@ -56,81 +57,81 @@ export default function Device() {
     }
 
     return (
-        <div>
-        <h1>{device.name}</h1>
-        <p>Type: {device.type}</p>
-        <p>Status: {device.status}</p>
+        <div className='device-details'>
+            <h1>{device.name}</h1>
+            <p>Type: {device.type}</p>
+            <p>Status: {device.status}</p>
 
-        {device.type === 'light' && (
-            <>
-            <p>Brightness: {device.brightness}</p>
-            <input
-                type="number"
-                value={device.brightness}
-                min="1"
-                max="100"
-                onChange={(e) => handleChange('brightness', e.target.value)}
-            />
-            </>
-        )}
+            {device.type === 'light' && (
+                <>
+                <p>Brightness: {device.brightness}</p>
+                <input
+                    type="number"
+                    value={device.brightness}
+                    min="1"
+                    max="100"
+                    onChange={(e) => handleChange('brightness', e.target.value)}
+                />
+                </>
+            )}
 
-        {device.type === 'thermostat' && (
-            <>
-            <p>Temperature: {device.temperature}°C</p>
-            <input
-                type="number"
-                value={device.temperature}
-                onChange={(e) => handleChange('temperature', e.target.value)}
-            />
-            </>
-        )}
+            {device.type === 'thermostat' && (
+                <>
+                <p>Temperature: {device.temperature}°C</p>
+                <input
+                    type="number"
+                    value={device.temperature}
+                    onChange={(e) => handleChange('temperature', e.target.value)}
+                />
+                </>
+            )}
 
-        {device.type === 'sound' && (
-            <>
-            <p>Volume: {device.volume}</p>
-            <input
-                type="number"
-                value={device.volume}
-                min="1"
-                max="100"
-                onChange={(e) => handleChange('volume', e.target.value)}
-            />
-            </>
-        )}
+            {device.type === 'sound' && (
+                <>
+                <p>Volume: {device.volume}</p>
+                <input
+                    type="number"
+                    value={device.volume}
+                    min="1"
+                    max="100"
+                    onChange={(e) => handleChange('volume', e.target.value)}
+                />
+                </>
+            )}
 
-        {device.type === 'accessory' && (
-            <>
-            <p>Voulume: {device.volume}</p>
-            <input
-                type="number"
-                value={device.volume}
-                min="1"
-                max="100"
-                onChange={(e) => handleChange('volume', e.target.value)}
-            />
-            <p>Channel: {device.channel}</p>
-            <input
-                type="number"
-                value={device.channel}
-                onChange={(e) => handleChange('channel', e.target.value)}
-            />
-            </>
-        )}
+            {device.type === 'accessory' && (
+                <>
+                <p>Voulume: {device.volume}</p>
+                <input
+                    type="number"
+                    value={device.volume}
+                    min="1"
+                    max="100"
+                    onChange={(e) => handleChange('volume', e.target.value)}
+                />
+                <p>Channel: {device.channel}</p>
+                <input
+                    type="number"
+                    value={device.channel}
+                    onChange={(e) => handleChange('channel', e.target.value)}
+                />
+                </>
+            )}
 
-        {device.type === 'fan' && (
-            <>
-            <p>Speed: {device.speed}</p>
-            <input
-                type="number"
-                value={device.speed}
-                min="1"
-                max="3"
-                onChange={(e) => handleChange('speed', e.target.value)}
-            />
-            </>
-        )}
+            {device.type === 'fan' && (
+                <>
+                <p>Speed: {device.speed}</p>
+                <input
+                    type="number"
+                    value={device.speed}
+                    min="1"
+                    max="3"
+                    onChange={(e) => handleChange('speed', e.target.value)}
+                />
+                </>
+            )}
 
-        <button onClick={handleSave}>Save changes</button>
+            <button onClick={handleSave}>Save changes</button>
         </div>
     );
-    }
+}
